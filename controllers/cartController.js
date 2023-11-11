29c64a15a395db1f58df5a4f3324e6d3ec0ffd24
sourabh@32 <sourabhsharma181003@gmail.com>
2023-11-11 16:07:14 +0530

    refactored logic in additem controller

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -8,24 +8,38 @@ const getCartItems = asyncHandler(async (req, res) => {
 
   res.status(200).json({ userId });
 });
+
 const addItemCart = asyncHandler(async (req, res) => {
   const { userId, productId } = req.body;
 
   try {
-    const cart = await Cart.findOne({ userId });
+    let cart = await Cart.findOne({ userId });
     const product = await Product.findById(productId);
 
-    if (!cart || !product) {
-      return res
-        .status(404)
-        .json({ message: "Resource not found", error: true });
+    if (!product) {
+      return res.status(404).json({ message: "Product not found", error: true });
+    }
+
+    if (!cart) {
+      const newCart = await Cart.create({
+        userId,
+        products: [
+          {
+            name: product.brand,
+            productId,
+            quantity: 1,
+            sum: product.price,
+          },
+        ],
+      });
+      return res.status(200).json(newCart);
     }
 
     const cartItem = cart.products.find((item) => item.productId === productId);
     if (cartItem) {
       cartItem.quantity += 1;
       await cart.save();
-      res.send(cart);
+      return res.send(cart);
     }
 
     const newItem = {
@@ -35,50 +49,15 @@ const addItemCart = asyncHandler(async (req, res) => {
       sum: product.price,
     };
 
-    if (cart) {
-      cart.products.push(newItem);
-      await cart.save();
-      res.status(200).json(cart);
-    } else {
-      const newCart = await Cart.create({
-        userId,
-        products: [newItem],
-      });
-      res.status(200).json(newCart);
-    }
+    cart.products.push(newItem);
+    await cart.save();
+    res.status(200).json(cart);
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: "Server error", error: true });
   }
 });
 
-const removeItemCart = asyncHandler(async (req, res) => {
-  const { userId, productId } = req.body;
-
-  try {
-    const cart = await Cart.findOne({ userId });
-    const product = await Product.findById(productId);
-
-    if (!cart || !product) {
-      return res
-        .status(404)
-        .json({ message: "Resource not found", error: true });
-    }
-    const cartItem = cart.products.find((item) => item.productId === productId);
-
-    if (cartItem) {
-      cart.products = cart.products.filter(
-        (item) => item.productId !== productId
-      );
-      await cart.save();
-      res.status(200).json(cart);
-    } else {
-      res.status(400).json({ message: "Item not Found!", error: true });
-    }
-  } catch (error) {
-    res.status(400).json({ message: error.meassage, error: true });
-  }
-});
 
 
 
